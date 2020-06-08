270f132e17b6ca789edbc565b0758b7f45f14827
Sol Abrahams <solomonabrahams100@gmail.com>
2020-06-08 15:15:28 +0100

    funny image added

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './header.css'
 
 class Header extends React.Component {
     render() {
         return(
-            <h1 style={{textAlign: "center"}}>The Salt Beef League</h1>
+            <div class='row' id='header-row'>
+                <img alt='' id='salt' src="https://img.icons8.com/emoji/96/000000/salt-emoji.png"/>
+                <h1 style={{textAlign: "center"}}>The Salt Beef League</h1>
+                <img alt='' id='beef' src="https://img.icons8.com/officel/80/000000/steak-medium.png"/>
+            </div>
         )
     }
 }
