19529740f32cb2770a954734e0ea95c235638c0a
Sol Abrahams <solomonabrahams100@gmail.com>
2020-06-15 18:31:52 +0100

    new path for production

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -11,4 +11,17 @@ module.exports = function(app) {
       }
     })
   );
+  app.use(
+    '/game/football',
+    createProxyMiddleware({
+      target: 'https://footballapi.pulselive.com',
+      pathRewrite: {
+        '^/game/football':'/football' //remove /service/api
+      },
+      changeOrigin: true,
+      headers: {
+        Origin: 'https://www.premierleague.com'
+      }
+    })
+  )
 };
