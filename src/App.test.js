109787dae91aacb2e17a569b59ad0092bb4db49a
Sol Abrahams <solomonabrahams100@gmail.com>
2020-07-01 17:19:37 +0100

    new tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText('Salt Beef League');
+  const linkElement = getByText('Sol');
   expect(linkElement).toBeInTheDocument();
 });
