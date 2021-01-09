2e396b7b195a5113469a89a0c071c634e8dc10af
sol <solomonabrahams100@gmail.com>
2021-01-09 14:07:10 +0000

    MAIN-14 #comment fixes environment variable name

--- src/globals.js
+++ src/globals.js
@@ -1,3 +1,2 @@
-console.log(process.env)
-const base_url = process.env.BASE_URL || 'http://192.168.0.16:5000'
+const base_url = process.env.REACT_APP_BASE_URL || 'http://192.168.0.16:5000'
 export default base_url
