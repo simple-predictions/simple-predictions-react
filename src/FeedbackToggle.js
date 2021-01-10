55a30c301038178eedd3d75b2caf134e3d11f637
sol <solomonabrahams100@gmail.com>
2021-01-10 22:42:49 +0000

    responsive feedback button

--- src/FeedbackToggle.js
+++ src/FeedbackToggle.js
@@ -4,8 +4,13 @@ import {withRouter} from 'react-router-dom'
 
 const FeedbackToggle = (props) => {
   return (
-    <div style={{backgroundColor: (window.location.pathname === '/' && props.apiCookie) ? 'black' : '', color: (window.location.pathname === '/' && props.apiCookie) ? '#defc5f' : ''}} onClick={() => {props.onTogglePopup()}} className='feedback-toggle'>
-      <div>Feedback</div>
+    <div>
+      <div style={{backgroundColor: (window.location.pathname === '/' && props.apiCookie) ? 'black' : '', color: (window.location.pathname === '/' && props.apiCookie) ? '#defc5f' : ''}} onClick={() => {props.onTogglePopup()}} className='feedback-toggle'>
+        <div>Feedback</div>
+      </div>
+      <div style={{borderBottomColor: (window.location.pathname === '/' && props.apiCookie) ? 'black' : '', color: (window.location.pathname === '/' && props.apiCookie) ? '#defc5f' : ''}} onClick={() => {props.onTogglePopup()}} className='feedback-toggle-corner'>
+        <div className='feedback-corner-text'>Feedback</div>
+      </div>
     </div>
   )
 }
