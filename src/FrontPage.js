e68709b0d4c234ed601755c3d739a14295b6fa37
sol <solomonabrahams100@gmail.com>
2021-01-16 23:33:55 +0000

    front page updates

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -6,15 +6,52 @@ import Register from './Register'
 import ResetPassword from './ResetPassword'
 import CreateNewPassword from './CreateNewPassword'
 import { Button } from 'react-bootstrap'
+import $ from 'jquery';
+import { set } from 'js-cookie'
+
 
 const FrontPage = ({widget, location}) => {
   const [popupOpen, setPopupOpen] = useState(false)
+  const [buttonBackgroundColor, setButtonBackgroundColor] = useState('#defc5f')
+
+  const handleScroll = (e) => {
+    const yellowBackgroundHeight = $('.three-steps-row').offset().top
+    const blackBackgroundHeight = $('.outer-about-row-container').offset().top
+    const currentScrollHeight = $(window).scrollTop()
+    const windowHeight = $(window).height()
+    const windowWidth = $(window).width()
+    if (windowWidth > 600) {
+      if ((currentScrollHeight+80 > yellowBackgroundHeight/2) && (currentScrollHeight+50 < blackBackgroundHeight-yellowBackgroundHeight/2)) {
+        $('.feedback-toggle').css('background-color', 'black')
+        $('.feedback-toggle').css('color', '#defc5f')
+      } else {
+        $('.feedback-toggle').css('background-color', '#defc5f')
+        $('.feedback-toggle').css('color', 'black')
+      }
+    } else {
+      if (currentScrollHeight > 50 && currentScrollHeight-50 < blackBackgroundHeight-windowHeight) {
+        $('.feedback-toggle-corner').css('border-bottom-color', 'black')
+        $('.feedback-toggle-corner').css('color', '#defc5f')
+      } else {
+        $('.feedback-toggle-corner').css('border-bottom-color', '#defc5f')
+        $('.feedback-toggle-corner').css('color', 'black')
+      }
+    }
+
+    if ((currentScrollHeight+50 > yellowBackgroundHeight) && (currentScrollHeight+50 < blackBackgroundHeight)) {
+      setButtonBackgroundColor('black')
+    } else {
+      setButtonBackgroundColor('#defc5f')
+    }
+  }
+
+  window.addEventListener("scroll", handleScroll)
 
   return (
     <div className="front-page-outer-container">
       {widget === 'Login' ? <LoginPage popupOpen={popupOpen} setPopupOpen={setPopupOpen} /> : widget === 'Register' ? <Register popupOpen={popupOpen} setPopupOpen={setPopupOpen} /> : widget === 'Reset Password' ? <ResetPassword /> : <CreateNewPassword location={location} />}
       <div className='login-button-outer-container'>
-        <Button style={{backgroundColor: '#defc5f', color: 'black'}} onClick={() => setPopupOpen(true)}>{widget}</Button>
+        <Button className='btn-lg' style={{backgroundColor: buttonBackgroundColor, color: buttonBackgroundColor === 'black' ? '#defc5f' : 'black'}} onClick={() => setPopupOpen(true)}>{widget}</Button>
       </div>
       <div className="front-page-top">
         <div className='text-container'>
@@ -24,52 +61,41 @@ const FrontPage = ({widget, location}) => {
       </div>
       <div className='front-page-scrollover'>
         <div className='row m-0 three-steps-row'>
-          <div className='step-number-row-container'>
-            <div className='col-md-4 three-steps-step'>
-              <div className='step-number-outer-container'>
-                <div className='step-number-container'>01</div>
-                <hr className='step-divider' />
-              </div>
+          <div className='col-lg-4 three-steps-step'>
+            <div className='step-number-outer-container'>
+              <div className='step-number-container'>01</div>
+              <hr className='step-divider' />
             </div>
-            <div className='col-md-4 three-steps-step'>
-              <div className='step-number-outer-container'>
-                <div className='step-number-container'>02</div>
-                <hr className='step-divider' />
-              </div>
-            </div>
-            <div className='col-md-4 three-steps-step'>
-              <div className='step-number-outer-container'>
-                <div className='step-number-container'>03</div>
-                <hr className='step-divider' />
-              </div>
+            <div className='step-text-outer-container'>
+              <div className='step-title-container'>Predict</div>
+              <p className='step-description-container'>Predict the outcome of all the Premier League fixtures each week.</p>
             </div>
           </div>
-
-          <div className='step-text-row-container'>
-            <div className='col-md-4 three-steps-step three-steps-step-text'>
-              <div className='step-text-outer-container'>
-                <div className='step-title-container'>Predict</div>
-                <p className='step-description-container'>Predict the outcome of all the Premier League fixtures each week.</p>
-              </div>
+          <div className='col-lg-4 three-steps-step'>
+            <div className='step-number-outer-container'>
+              <div className='step-number-container'>02</div>
+              <hr className='step-divider' />
             </div>
-            <div className='col-md-4 three-steps-step three-steps-step-text'>
-              <div className='step-text-outer-container'>
-                <div className='step-title-container'>Score</div>
-                <p className='step-description-container'>Watch your points live as your predictions are scored as the goals come in.</p>
-              </div>
+            <div className='step-text-outer-container'>
+              <div className='step-title-container'>Score</div>
+              <p className='step-description-container'>Watch your points accumulate as the goals come in.</p>
             </div>
-            <div className='col-md-4 three-steps-step three-steps-step-text'>
-              <div className='step-text-outer-container'>
-                <div className='step-title-container'>Compete</div>
-                <p className='step-description-container'>Join a mini-league to track your progress against your competitors throughout the season.</p>
-              </div>
+          </div>
+          <div className='col-lg-4 three-steps-step'>
+            <div className='step-number-outer-container'>
+              <div className='step-number-container'>03</div>
+              <hr className='step-divider' />
+            </div>
+            <div className='step-text-outer-container'>
+              <div className='step-title-container'>Compete</div>
+              <p className='step-description-container'>Join a mini-league to track your progress against competitors throughout the season.</p>
             </div>
           </div>
         </div>
         <div className='row m-0 outer-about-row-container'>
-          <div className='about-row'>
-            <div className='col-md-2'></div>
-            <div className='about-row-image-container col-md-2'>
+          <div className='about-row row'>
+            <div className='col-lg-2'></div>
+            <div className='about-row-image-container col-lg-2'>
               <svg className='about-row-icon-hex-outline' viewBox='0 0 120 100'>
                 <path d='M38,2 
                         L82,2 
@@ -87,15 +113,15 @@ const FrontPage = ({widget, location}) => {
               </svg>
               <img src={require('./icons/book.png')} className='about-row-icon' />
             </div>
-            <div className='about-row-text-container col-md-6'>
+            <div className='about-row-text-container col-lg-6'>
               <div className='about-row-header-container'>Our story</div>
-              <div className='about-row-description-container'>Here is our story.</div>
+              <div className='about-row-description-container'>The Salt Beef League began as an annual competition between two families on the now discontinued talkSport predictor site. The first iteration of my site used an API provided by talkSport to get users' predictions and displayed them in a clear table. It also scored the games live as the goals came in. This latest iteration, which I'm sure won't be the last, is a standalone website which doesn't depend on talkSport. </div>
             </div>
-            <div className='col-md-2'></div>
+            <div className='col-lg-2'></div>
           </div>
-          <div className='about-row'>
-            <div className='col-md-2'></div>
-            <div className='about-row-image-container col-md-2'>
+          <div className='about-row row'>
+            <div className='col-lg-2'></div>
+            <div className='about-row-image-container col-lg-2'>
               <svg className='about-row-icon-hex-outline' viewBox='0 0 120 100'>
                 <path d='M38,2 
                         L82,2 
@@ -113,15 +139,15 @@ const FrontPage = ({widget, location}) => {
               </svg>
               <img src={require('./icons/eye.png')} className='about-row-icon' />
             </div>
-            <div className='about-row-text-container col-md-6'>
+            <div className='about-row-text-container col-lg-6'>
               <div className='about-row-header-container'>Our vision</div>
-              <div className='about-row-description-container'>Here is our vision.</div>
+              <div className='about-row-description-container'>I haven't yet decided the future of this site, however at the moment I don't plan on running it full-time in production. It is a showcase of my skills and shows that I can think creatively to problem solve. For example, I haven't paid for any live data and the live scoring works by parsing the Premier League tweets from the Twitter API. I'm keen to get as much feedback and user input as possible to improve this Alpha version further.</div>
             </div>
-            <div className='col-md-2'></div>
+            <div className='col-lg-2'></div>
           </div>
-          <div className='about-row'>
-            <div className='col-md-2'></div>
-            <div className='about-row-image-container col-md-2'>
+          <div className='about-row row'>
+            <div className='col-lg-2'></div>
+            <div className='about-row-image-container col-lg-2'>
               <svg className='about-row-icon-hex-outline' viewBox='0 0 120 100'>
                 <path d='M38,2 
                         L82,2 
@@ -139,11 +165,11 @@ const FrontPage = ({widget, location}) => {
               </svg>
               <img src={require('./icons/person.png')} className='about-row-icon' />
             </div>
-            <div className='about-row-text-container col-md-6'>
+            <div className='about-row-text-container col-lg-6'>
               <div className='about-row-header-container'>About me</div>
-              <div className='about-row-description-container'>Sol Abrahams.</div>
+              <div className='about-row-description-container'>I'm a Year 12 student with a passion for programming and problem solving. As my only formal coding education has been a work experience placement at Forensic Architecture, I am entirely self-taught and have learnt by practise and experimentation, such as this site. If you'd like to find out more about me, please go to my <a className='personal-site-link' href='https://www.solabrahams.tech'>personal site.</a></div>
             </div>
-            <div className='col-md-2'></div>
+            <div className='col-lg-2'></div>
           </div>
         </div>
       </div>
