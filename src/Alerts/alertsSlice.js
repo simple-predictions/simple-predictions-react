545dcd22a1e857a7ac175a7593ebe84d44628b26
sol <solomonabrahams100@gmail.com>
2021-01-18 13:43:34 +0000

    lint init

--- src/Alerts/alertsSlice.js
+++ src/Alerts/alertsSlice.js
@@ -1,25 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 export const alertsSlice = createSlice({
   name: 'alerts',
   initialState: {
     alertMessage: '',
-    alertVariant: ''
+    alertVariant: '',
   },
   reducers: {
     updateAlert: (state, action) => {
-      state.alertMessage = action.payload.message
-      state.alertVariant = action.payload.variant || 'success'
+      state.alertMessage = action.payload.message;
+      state.alertVariant = action.payload.variant || 'success';
     },
-    clearAlert: state => {
+    clearAlert: (state) => {
       state.alertMessage = ''
     }
   }
 })
 
-export const { updateAlert, clearAlert } = alertsSlice.actions
+export const { updateAlert, clearAlert } = alertsSlice.actions;
 
-export const selectAlertMessage = state => state.alerts.alertMessage
-export const selectAlertVariant = state => state.alerts.alertVariant
+export const selectAlertMessage = (state) => state.alerts.alertMessage;
+export const selectAlertVariant = (state) => state.alerts.alertVariant;
 
-export default alertsSlice.reducer
+export default alertsSlice.reducer;
--- src/Feedback/feedbackSlice.js
+++ src/Feedback/feedbackSlice.js
@@ -1,18 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 export const feedbackSlice = createSlice({
   name: 'feedback',
   initialState: {
-    popupOpen: false
+    popupOpen: false,
   },
   reducers: {
-    openPopup: state => {state.popupOpen = true},
-    closePopup: state => {state.popupOpen = false}
-  }
-})
+    openPopup: (state) => {state.popupOpen = true},
+    closePopup: (state) => {state.popupOpen = false}
+  },
+});
 
-export const { openPopup, closePopup } = feedbackSlice.actions
+export const { openPopup, closePopup } = feedbackSlice.actions;
 
-export const selectPopupOpen = state => state.feedback.popupOpen
+export const selectPopupOpen = (state) => state.feedback.popupOpen;
 
-export default feedbackSlice.reducer
+export default feedbackSlice.reducer;
