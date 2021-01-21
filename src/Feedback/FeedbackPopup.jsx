0f85a86522afa6ad6cd72898b89306aba807a407
sol <solomonabrahams100@gmail.com>
2021-01-21 23:19:55 +0000

    feedback sends with ip address

--- src/Feedback/FeedbackPopup.jsx
+++ src/Feedback/FeedbackPopup.jsx
@@ -3,6 +3,7 @@ import {
   FormGroup, Button, FormControl, FormLabel,
 } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
+import publicIp from 'public-ip';
 import baseUrl from '../globals';
 import { selectPopupOpen, closePopup } from './feedbackSlice';
 import { selectUserUsername, selectUserEmail } from '../User/userSlice';
@@ -15,9 +16,17 @@ const FeedbackPopup = () => {
   const [summary, setSummary] = useState('');
   const [description, setDescription] = useState('');
   const [buttonEnabled, setButtonEnabled] = useState(true);
+  const [ipAddress, setipAddress] = useState('');
   const username = useSelector(selectUserUsername);
   const email = useSelector(selectUserEmail);
   const dispatch = useDispatch();
+
+  const setIP = async () => {
+    const ip = await publicIp.v4();
+    setipAddress(ip);
+  };
+
+  setIP();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -55,6 +64,7 @@ const FeedbackPopup = () => {
         customfield_10031: username,
         customfield_10033: email,
         customfield_10029: window.location.pathname,
+        customfield_10036: ipAddress,
         issuetype: {
           name: issuetype,
         },
