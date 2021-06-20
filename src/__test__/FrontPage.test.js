ac44ef359a2fb65cad0c7e316f8c79fdf520b61c
Sol Abrahams <solomonabrahams100@gmail.com>
2021-06-20 20:01:43 +0100

    front page tests updates

--- src/__test__/FrontPage.test.js
+++ src/__test__/FrontPage.test.js
@@ -1,30 +1,29 @@
 /* eslint-disable */
 import React from "react";
-import { act } from 'react-dom/test-utils';
-import { render, unmountComponentAtNode } from "react-dom";
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import FrontPage from '../FrontPage';
 import store from '../store/store';
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom';
-
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+import '@testing-library/jest-dom/extend-expect'
 
 describe('test front page', function() {
-    it("should have a title", function() {
-        act(() => {
-            render(<Provider store={store}><Router><FrontPage widget="Login" /></Router></Provider>, container)
-        })
+    it("should update widget", async function() {
+        const { rerender } = render(<Provider store={store}><Router><FrontPage widget="Login" /></Router></Provider>)
+        const items = await screen.findAllByText("Login")
+        expect(items).toHaveLength(2)
+
+        rerender(<Provider store={store}><Router><FrontPage widget="Register" /></Router></Provider>)
+        await screen.findByText("Login")
+        await screen.findByText("Register")
+        await screen.findByText("Sign up")
+
+        rerender(<Provider store={store}><Router><FrontPage widget="Reset Password" /></Router></Provider>)
+        await screen.findByPlaceholderText("Username")
+        await screen.findByText("Reset")
+
+        rerender(<Provider store={store}><Router><FrontPage widget="Create new password" /></Router></Provider>)
+        await screen.findByPlaceholderText("New password")
+        await screen.findByText("Reset")
     })
 })
